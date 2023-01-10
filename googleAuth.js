44445b0f10e2cb61fffa6ba03eee6db56796c5bb
unknown <freelancestanislav@gmail.com>
2023-01-10 20:57:57 +0300

    start

---
+++ googleAuth.js
@@ -0,0 +1,42 @@
+const { google } = require('googleapis');
+const config = require('config');
+
+const scopes = config.get("scopes")
+const spreadsheetId = config.get("spreadsheetId")
+
+const auth = new google.auth.GoogleAuth({
+    keyFile: "credentials.json",
+    scopes,
+})
+
+async function getClient() {
+    const client = await auth.getClient()
+    return client
+}
+
+async function getSheets() {
+    const client = await getClient()
+    const sheets = google.sheets({ version: "v4", auth: client })
+    return sheets
+}
+
+async function getMetaData() {
+    const metadata = await (await getSheets()).spreadsheets.get({
+        auth,
+        spreadsheetId,
+    })
+
+    return metadata
+}
+
+async function getRows({ range }) {
+    const rows = await (await getSheets()).spreadsheets.values.get({
+        auth,
+        spreadsheetId,
+        range
+    })
+
+    return rows
+}
+
+module.exports = { getClient, getSheets, getMetaData, getRows }
---
+++ server.js
@@ -0,0 +1,77 @@
+const TelegramBot = require('node-telegram-bot-api');
+const config = require('config');
+const { getRows } = require("./googleAuth")
+
+const TOKEN = process.env.TOKEN || config.get("TOKEN")
+const bot = new TelegramBot(TOKEN, { polling: true });
+
+
+const regularOptions = {
+    start: /\/start/,
+    code: /([0-9]+|secret)/,
+    help: /\/help/
+}
+
+async function start() {
+    bot.onText(regularOptions.start, message => {
+        greeting(message)
+    })
+
+    bot.onText(regularOptions.code, async message => {
+        const isSubscribe = checkSubscribe()
+
+        if (isSubscribe) {
+            const response = await getRows({ range: "data" })
+            const data = formatResponse(response)
+            if (data[message.text]) {
+                bot.sendMessage(message.chat.id, data[message.text])
+            } else {
+                bot.sendMessage(message.chat.id, `Фильм/Сериал по коду '${message.text}' не найден.`)
+            }
+            
+        } else {
+            bot.sendMessage(message.chat.id, "Чтобы бот работал ван нужно подписаться на каналы:")
+        }
+    })
+
+    bot.onText(regularOptions.help, message => {
+        help(message)
+    })
+
+    bot.on("message", message => {
+        let notFound = 0
+
+        for (key in regularOptions) {
+            notFound += !regularOptions[key].test(message.text) ? 1 : 0
+        }
+
+        if (notFound >= Object.keys(regularOptions).length) {
+            console.log(message.text);
+            bot.sendMessage(message.chat.id, "Простите, я вас не понимаю.\nВведите /help чтобы узнать, что я умею.", { parse_mode: "HTML" })
+        }
+    })
+}
+
+function greeting(message) {
+    bot.sendMessage(message.chat.id, `Привет, ${message.from.first_name}!`)
+}
+
+function help(message) {
+    bot.sendMessage(message.chat.id, "Я умею отправлять названия фильмов/сериалов. Просто введите код фильма/сериала, например \"123\"")
+}
+
+start()
+
+function checkSubscribe() {
+    return true
+}
+
+function formatResponse(response) {
+    return response.data.values.reduce((acc, cur, index) => {
+        if (index === 0) {
+            return acc
+        }
+
+        return { ...acc, [cur[0]]:cur[1] }
+    }, {})
+}
